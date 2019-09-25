a74c4cbbc0ee9deef511291c8daaba5e5a62d982
Jeferson.Rocha <tecjeferson@gmail.com>
2019-09-24 23:01:53 -0300

    Working on assPayment at Subscription.cs 20m

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace StudentTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AdicionarAssinatura()
+        {
+            var subscription = new Subscription();
+            var student = new Student("Ryan", "Vieira de Vargas", "222916014", "ryan@gmail.com");
+            student.AddSubscription(subscription);
+        }
+    }
+}
